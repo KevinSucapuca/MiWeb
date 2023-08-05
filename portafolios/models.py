5a374faefb052d365f81ca125362a0de2101106b
KevinSucapuca <ksucapuca007@gmail.com>
2023-08-05 16:30:05 -0500

    add modles Proyecto and contacto

--- portafolios/models.py
+++ portafolios/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Proyecto(models.Model):
+    nombre_proyecto = models.CharField(max_length=70)
+    tipo_proyecto = models.CharField(max_length=70)
+    descripcion_proyecto = models.TextField(max_length=500)
+    url_imagen = models.URLField(max_length=250)
+    url_demo = models.URLField(max_length=250)
+    url_github = models.URLField(max_length=250)
+    prioridad = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.nombre_proyecto} - {self.tipo_proyecto}"
+    
+class ContactMessage(models.Model):
+    contact_name = models.CharField(max_length=100)
+    contact_email = models.EmailField()
+    contact_subject = models.CharField(max_length=200, blank=True)
+    contact_message = models.TextField()
+
+    def __str__(self):
+        return f"{self.contact_name} - {self.contact_subject}"
+        
