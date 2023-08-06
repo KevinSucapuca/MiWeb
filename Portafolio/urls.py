b042b2ae9c2f99cd59a8f9a41a7e8a2467635914
KevinSucapuca <ksucapuca007@gmail.com>
2023-08-06 01:14:29 -0500

    add url for controlar error 404

--- Portafolio/urls.py
+++ Portafolio/urls.py
@@ -1,8 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls import handler404
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('adminportafolio/', admin.site.urls),
     path('', include('portafolios.urls')),
 ]
+
+
+handler404 = 'portafolios.views.custom_404'
